0110fcd8c6a3cbdf677f4b64c0a52dbcb7c881e1
Evgeny Aksenov <e.aksenov@gmail.com>
2019-01-23 23:57:50 +0300

    #3 Basic sprite and text

--- js-pixi/src/pixi/core.cljs
+++ js-pixi/src/pixi/core.cljs
@@ -9,8 +9,31 @@
 
 (defonce renderer
          (.autoDetectRenderer 
-           js/PIXI 800 600
+           js/PIXI 600 400
            #js {:view (.getElementById js/document "game-canvas")}))
+
+(def white-text (js/PIXI.TextStyle. #js {:fontSize 36 
+										 :fill #js ["red" "#000000"]}))
+(def hello 
+	(doto (js/PIXI.Text. "Hello world" white-text)
+		(aset "x" 100)
+		(aset "y" 200)))
+
+
+(defn completed-loading-resources!
+  [loader resources]
+  (println "Resources loaded!")
+  (let [skull (js/PIXI.Sprite. (aget resources "skull" "texture"))]
+    (.addChild container skull)
+    (.addChild container hello)
+    (.render renderer container)))
+
+(defn load-resources! []
+  (-> (js/PIXI.loaders.Loader.)
+      (.add "skull" "assets/img/skull2.png")
+      (.load completed-loading-resources!)))
+
+(load-resources!)
 
 ; var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
 ; document.body.appendChild(app.view);
